d7a2e61035cadd303d3c555189449248e0c3cb22
Sebastian Webber <swebber@outlook.com>
2015-03-09 11:15:16 -0300

    add missing log_format on pgbadger usage and changing version

--- js/app.js
+++ js/app.js
@@ -60,7 +60,7 @@ pgConfigApp.controller('ConfigurationController', function ($scope, $http, $filt
   };
 
 
-  $scope.version = '1.2';
+  $scope.version = '1.25';
 
   $http.get('data/pgsql-parameters.json').success(function(data) {
     $scope.pgsql_parameters = data;
