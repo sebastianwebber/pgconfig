13f7557e3a61b58c5514a56f8f3660db2894c350
Sebastian Webber <sebastian@swebber.me>
2016-03-18 11:39:31 -0300

    adding old and still supported versions

--- js/app.js
+++ js/app.js
@@ -91,7 +91,7 @@ pgConfigApp.controller('ConfigurationController', function ($scope, $http, $filt
   };
 
 
-  $scope.version = '1.35';
+  $scope.version = '1.40';
 
   $scope.showAtVersion = function(currentVersion, minimumVersion, maximunVersion) {
     // console.info("currentVersion: " + currentVersion) + "--" + "minimumVersion: " + minimumVersion;
