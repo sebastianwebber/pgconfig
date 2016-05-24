777f8fb9c3530c7b324b5956df3c9270b2384a04
Sebastian Webber <sebastian@swebber.me>
2016-05-24 13:00:26 -0300

    adding cache workaround, custom routing for tuning

--- app/main.js
+++ app/main.js
@@ -34,12 +34,27 @@
                 }
             });
         })
+        // cache stuff
+        .run(['$templateCache', function ($templateCache) {
+            $templateCache.removeAll();
+        }])
 
-        .controller('TuningController', function ($scope, $location, $log, $http, $resource, $mdSidenav, $mdComponentRegistry) {
-
+        .controller('TuningController', function ($scope, $location, $log, $http, $resource, $mdSidenav, $routeParams) {
+                    
+    
             $scope.total_memory = 2;
             $scope.max_connections = 100;
             $scope.pg_version = "9.5";
+            $scope.enviroment = "WEB";
+            
+            if($routeParams.total_ram != null) 
+                $scope.total_memory = Number($routeParams.total_ram);
+            if($routeParams.max_connections != null)
+                $scope.max_connections = Number($routeParams.max_connections);
+            if($routeParams.pg_version != null)
+                $scope.pg_version = $routeParams.pg_version;
+            if($routeParams.env_name != null)
+                $scope.enviroment = $routeParams.env_name;
 
             $scope.supported_versions = [
                 "9.5",
@@ -47,9 +62,28 @@
                 "9.3",
                 "9.2",
                 "9.1",
-                "9.0",
             ];
 
+            $scope.show_toolbar = false;
+
+            $scope.envFilters = [
+                {
+                    value: "WEB",
+                    description: 'For general web applications',
+                }, {
+                    value: "OLTP",
+                    description: 'For ERP or long transaction applications',
+                }, {
+                    value: "DW",
+                    description: 'For DataWare house and BI Applications',
+                }, {
+                    value: "Mixed",
+                    description: 'For DB and APP on the same server',
+                }, {
+                    value: "Desktop",
+                    description: 'For developer local machine',
+                },
+            ];
 
             var TuningAPI = $resource("http://api.pgconfig.org/v1/tuning/get-config", {});
 
@@ -59,16 +93,20 @@
                         // $log.debug("close LEFT is done");
                     });
             };
-            
+
+
             $scope.call_api = function () {
                 TuningAPI.get({
-                    pg_version : $scope.pg_version,
-                    total_ram : $scope.total_memory + "GB",
-                    max_connections : $scope.max_connections ,
-                    format : "json"
+                    pg_version: $scope.pg_version,
+                    total_ram: $scope.total_memory + "GB",
+                    max_connections: $scope.max_connections,
+                    env_name: $scope.enviroment,
+                    format: "json",
+                    show_doc: true
                 }, function (apiResult) {
                     $scope.api_data = apiResult.data;
                     $scope.close();
+                    $scope.show_toolbar = true;
                 });
             };
         });
