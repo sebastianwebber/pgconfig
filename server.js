559455473f90efa677b8501d131b922024105fb0
Sebastian Webber <sebastian@swebber.me>
2016-05-25 21:04:44 -0300

    using express for open app with node

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+
+// set up ========================
+var express = require('express');
+var app = express();
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+// configuration =================
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+
+app.use(express.static(__dirname + '/app'));                 // set the static files location /public/img will be /img for users
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// listen (start app with node server.js) ======================================
+app.listen(server_port, server_ip_address, function(){
+      console.log( "Listening on http://" + server_ip_address + ":" + server_port + "/" )
+});
