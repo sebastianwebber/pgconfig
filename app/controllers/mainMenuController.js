45d5eb281496925ace367d7d740037ad6d387dfd
Sebastian Webber <sebastian@swebber.me>
2016-05-27 12:10:13 -0300

    adjusting routes and fix share url

--- app/controllers/mainMenuController.js
+++ app/controllers/mainMenuController.js
@@ -1,13 +1,13 @@
 function MainMenuController($scope, $location, $log) {
-    $scope.selectedIndex = 0;
-    $scope.$watch('selectedIndex', function (current, old) {
-        switch (current) {
-            case 0:
-                $location.url("/about");
-                break;
-            case 1:
-                $location.url("/tuning");
-                break;
-        }
-    });
+    // $scope.selectedIndex = 0;
+    // $scope.$watch('selectedIndex', function (current, old) {
+    //     switch (current) {
+    //         case 0:
+    //             $location.url("/about");
+    //             break;
+    //         case 1:
+    //             $location.url("/tuning");
+    //             break;
+    //     }
+    // });
 };
--- app/routes/mainRoute.js
+++ app/routes/mainRoute.js
@@ -1,16 +1,14 @@
 angular.module('pgconfig.routes', ['ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('about_url', {
-                url: "/about",
-                templateUrl: "partials/about.html"
-            })
             .state('tuning_url', {
                 url: "/tuning?total_ram&max_connections&enviroment_name&pg_version&share_link",
                 templateUrl: "partials/tuning.html"
             })
-            .state("otherwise", {
-                url: "*path",
-                templateUrl: "partials/about.html",
+            .state('about_url', {
+                url: "/about",
+                templateUrl: "partials/about.html"
             });
+            
+        $urlRouterProvider.otherwise('/tuning');
     });
