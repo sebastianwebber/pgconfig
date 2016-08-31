54213858b5a649a2427b7d84a872039153aac100
Sebastian Webber <sebastian@swebber.me>
2016-08-30 23:42:12 -0300

    allowing to toggle parameter documentation

--- src/app/controllers/parameterDirectiveController.js
+++ src/app/controllers/parameterDirectiveController.js
@@ -3,8 +3,13 @@ function ParameterDirectiveController($scope) {
     // $scope.app_desc = config.appFullName;
 
     $scope.show_details = false;
+    $scope.show_doc = false;
 
     $scope.toggle = function(){
         $scope.show_details = !$scope.show_details;
+    };
+
+    $scope.toggle_doc = function(){
+        $scope.show_doc = !$scope.show_doc;
     };
 };
