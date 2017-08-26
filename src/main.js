563e934888f198dceedc090f6d41b315ba79bde4
Ramon Schmidt <ramonroc@gmail.com>
2017-08-26 02:29:34 -0300

    Vuetify installed and added as a dependency in vue instance

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from './App';
 import router from './router';
 
+Vue.use(Vuetify);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
