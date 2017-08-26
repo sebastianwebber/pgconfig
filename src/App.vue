c592b233eccaf9dd853fe2b71cd10ec8f5f339d8
Ramon Schmidt <ramonroc@gmail.com>
2017-08-26 17:21:48 -0300

    Add material icons google

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,54 @@
 <template>
-<v-app toolbar>
-    <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
-      <v-toolbar flat class="transparent">
-        <v-list class="pa-0">
-          <v-list-tile avatar tag="div">
-            <v-list-tile-avatar>
-              <img src="https://randomuser.me/api/portraits/men/85.jpg" />
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>John Leider</v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-action>
-              <v-btn icon @click.native.stop="mini = !mini">
-                <v-icon>chevron_left</v-icon>
-              </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
-        <v-list-tile v-for="item in items" :key="item.title">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed class="indigo darken-4" dark>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Toolbar</v-toolbar-title>
-    </v-toolbar>
-    <main>
-      <v-container fluid>
-        <img src="./assets/logo.png">
-        <router-view></router-view>
-      </v-container>
-    </main>
-  </v-app>
+<!DOCTYPE html>
+<html>
+  <head>
+  <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+  </head>
+  <body>
+    <v-app id="app" toolbar>
+        <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
+          <v-toolbar flat class="transparent">
+            <v-list class="pa-0">
+              <v-list-tile avatar tag="div">
+                <v-list-tile-avatar>
+                  <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>John Leider</v-list-tile-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <v-btn icon @click.native.stop="mini = !mini">
+                    <v-icon>chevron_left</v-icon>
+                  </v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
+            </v-list>
+          </v-toolbar>
+          <v-list class="pt-0" dense>
+            <v-divider></v-divider>
+            <v-list-tile v-for="item in items" :key="item.title">
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-navigation-drawer>
+        <v-toolbar fixed class="indigo darken-4" dark>
+          <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+          <v-toolbar-title>Toolbar</v-toolbar-title>
+        </v-toolbar>
+        <main>
+          <v-container fluid>
+            <img src="./assets/logo.png">
+            <router-view></router-view>
+          </v-container>
+        </main>
+      </v-app>
+    </body>
+</html>
 </template>
 
 <script>
@@ -59,3 +67,6 @@
     },
   };
 </script>
+<style lang='stylus'>
+  @import './stylus/main'
+</style>
