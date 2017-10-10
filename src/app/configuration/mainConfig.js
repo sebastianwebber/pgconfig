e14aa202866a1a8396b362f01cd53f9bcb8c64ae
Sebastian Webber <sebastian@swebber.me>
2017-10-10 02:45:53 -0300

    updating new api url

--- src/app/configuration/mainConfig.js
+++ src/app/configuration/mainConfig.js
@@ -3,15 +3,15 @@ angular.module('pgconfig.config', [])
         appName: 'PGConfigUI',
         appFullName: 'PostgreSQL Configuration Tool',
         appVersion: '2.0 beta',
-        apiURL: 'https://api.pgconfig.org/v1',
+        apiURL: 'https://pgconfig.appspot.com/v1',
         // apiURL: 'http://localhost:5000/v1'
     })
-    .config(function ($mdThemingProvider) {
+    .config(function($mdThemingProvider) {
         $mdThemingProvider
             .theme('default')
             .primaryPalette('blue')
             .accentPalette('orange');
     })
-    .run(['$rootScope', function ($rootScope) {
+    .run(['$rootScope', function($rootScope) {
         $rootScope.angular = angular;
     }]);
